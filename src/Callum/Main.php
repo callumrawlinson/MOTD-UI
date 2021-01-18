2183797511e9fd2aece7865ed5de3ed778dc843f
Callum <72660075+callumrawlinson@users.noreply.github.com>
2021-01-18 09:46:42 +0000

    Added Main.php

---
+++ src/Callum/Main.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Callum;
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\plugin\PluginBase as Plugins;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\utils\Config;
+
+use DevAkya\Command\Motd;
+
+class Main extends PluginBase implements Listener {
+
+	const PLUGIN_NAME = "MotdUI";
+
+	public function onEnable() {
+		$this->getLogger()->info("§aON");
+		$this->getServer()->getPluginManager()->registerEvents($this,$this);
+		$this->getServer()->getCommandMap()->register("motd", new Motd($this));
+	}
+
+	public function onDisable() {
+		$this->getLogger()->info("§cOFF");
+	}
+}
